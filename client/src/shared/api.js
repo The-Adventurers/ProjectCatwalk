6b13827333629c605e7651ce57aabc3988f2d6f2
Justin Le <justinhung.le@gmail.com>
2021-12-22 18:24:38 -0800

    Reformatted code to follow Eslint

--- client/src/shared/api.js
+++ client/src/shared/api.js
@@ -1,35 +1,18 @@
 import axios from 'axios';
 
 export const getProducts = (productId) => {
-  if (id !== undefined) {
+  if (productId !== undefined) {
     return axios.get('/api/catwalk/products', {
-      params: { id: productId }
-    })
-  } else {
-    return axios.get('/api/catwalk/products')
-  };
+      params: { id: productId },
+    });
+  }
+  return axios.get('/api/catwalk/products');
 };
 
-export const getReviews = (productId) => {
-  return axios.get('/api/catwalk/reviews', {
-    params: { id: productId }
-  })
-};
+export const getReviews = (productId) => axios.get('/api/catwalk/reviews', { params: { id: productId } });
 
-export const getMeta = (productId) => {
-  return axios.get('/api/catwalk/meta', {
-    params: { id: productId }
-  })
-};
+export const getMeta = (productId) => axios.get('/api/catwalk/meta', { params: { id: productId } });
 
-export const getQuestions = (productId) => {
-  return axios.get('/api/catwalk/questions', {
-    params: { id: productId }
-  })
-};
+export const getQuestions = (productId) => axios.get('/api/catwalk/questions', { params: { id: productId } });
 
-export const getAnswers = (productId) => {
-  return axios.get('/api/catwalk/questions', {
-    params: { id: productId }
-  })
-};
+export const getAnswers = (productId) => axios.get('/api/catwalk/questions', { params: { id: productId } });
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,63 +1,63 @@
-const models = require('../models/models.js');
+const models = require('../models/models');
 
 module.exports = {
   getProducts: (req, res) => {
     const params = req.query.id;
     models.getProducts(params, (error, results) => {
-      if(error) {
+      if (error) {
         res.status(500).send(error);
       } else {
-        res.send(results)
-      };
+        res.send(results);
+      }
     });
   },
   getReviews: (req, res) => {
     const params = req.query.id;
     models.getReviews(params, (error, results) => {
-      if(error) {
+      if (error) {
         res.status(500).send(error);
       } else {
         res.send(results);
-      };
+      }
     });
   },
   getMeta: (req, res) => {
     const params = req.query.id;
     models.getMeta(params, (error, results) => {
-      if(error) {
+      if (error) {
         res.status(500).send(error);
       } else {
         res.send(results);
-      };
-    })
+      }
+    });
   },
   getQuestions: (req, res) => {
     const params = req.query.id;
     models.getQuestions(params, (error, results) => {
-      if(error) {
+      if (error) {
         res.status(500).send(error);
       } else {
-        res.send(results)
-      };
+        res.send(results);
+      }
     });
   },
   getAnswers: (req, res) => {
     const params = req.query.id;
     models.getAnswers(params, (error, results) => {
-      if(error) {
+      if (error) {
         res.status(500).send(error);
       } else {
-        res.send(results)
-      };
+        res.send(results);
+      }
     });
   },
   getCart: (req, res) => {
     models.getCart((error, results) => {
-      if(error) {
+      if (error) {
         res.status(500).send(error);
       } else {
-        res.send(results)
-      };
+        res.send(results);
+      }
     });
-  }
+  },
 };
