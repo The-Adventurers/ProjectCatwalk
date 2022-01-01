b137d0b2c697f5dbf25da6eb135b7f9ed45a380b
Jantira Vongampai <janvong.official@gmail.com>
2022-01-01 12:11:54 -0800

    partially finish Overlay modal for Add new Question

--- client/src/components/QA/QAsection.jsx
+++ client/src/components/QA/QAsection.jsx
@@ -43,7 +43,43 @@ const QAsection = ({ product_id }) => {
     <div className='main-QA'>
 
       <Questions questions={allQuestions} updateData={()=>setUpdate(!update)}/>
+      <div className='question-form-container'>
+        <div className='question-form'>
+
+          <div> Ask Your Question
+            <i className="far fa-window-close"></i>
           </div>
+
+          <h3>About the [product name....pass in later]</h3>
+
+          <label htmlFor='question-body'>Your Question :
+          <textarea id='question-body' name='question' placeholder='Why did you like the product or not?' maxLength='1000'>
+          </textarea>
+          </label>
+
+
+          <label htmlFor='question-nickname'><p>What is your nickname :</p>
+          <p>
+            <input id='question-nickname' placeholder='Example: jackson11!'></input>
+            <span>For privacy reasons, do not use your full name or email address</span>
+          </p>
+          </label>
+
+
+          <label htmlFor='question-email'><p>Your email :</p>
+          <p>
+            <input type='email' id='question-email' placeholder='Example@example.com' required></input>
+            <span>For authentication reasons, you will not be emailed</span>
+          </p>
+          </label>
+
+          <button>Submit</button>
+
+
+        </div>
+      </div>
+
+    </div>
   );
 };
 
