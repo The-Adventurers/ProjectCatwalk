ec515324fe7be1b5247991bd23e05e29efc68b26
one-summers-day <jiro.horikoshi1@gmail.com>
2022-01-02 23:12:29 -0600

    Some edits

--- client/src/components/R&R/RnRApp copy.jsx
+++ client/src/components/R&R/RnRApp copy.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import useWindowDimensions from '../../shared/SharedHooks/useWindowDimensions';
 import { MainContainer2, RelatedProducts, Carousel } from '../../../dist/RnRStyles';
-import { getReviews } from '../../shared/api';
 
 const RnRApp = function () {
   const { height, width } = useWindowDimensions();
   return (
     <MainContainer2>
     <div style = {{paddingTop : height * 0.05}}>
-         {getReviews( {product_id : 63609} ).then(
-           res => {console.log( res.data.results)})
-      } <span> Just a plain old right box. </span> 
+         <span> Just a plain old box. </span> <span> Just a plain old right box. </span> 
     </div>
     </MainContainer2>
   )
