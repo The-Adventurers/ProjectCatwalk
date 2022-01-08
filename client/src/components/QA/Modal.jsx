7ad115899406b7a648012dadd387c23d5f108066
Jantira Vongampai <janvong.official@gmail.com>
2022-01-08 14:13:36 -0800

    clean up log

--- client/src/components/QA/Modal.jsx
+++ client/src/components/QA/Modal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Modal = ({showPhoto, setShowPhoto, form}) => {
+const Modal = ({showPhoto, setShowPhoto}) => {
   const handleImgOnClick = (e) => {
     if(e.target.tagName === 'I') {
       const target = e.target.className;
@@ -9,15 +9,14 @@ const Modal = ({showPhoto, setShowPhoto, form}) => {
       } else if (target.includes('left') && showPhoto[0] > 0) {
         setShowPhoto([showPhoto[0] - 1, showPhoto[1]]);
       } else if (target.includes('right') && showPhoto[0] < showPhoto[1].length - 1) {
-        setShowPhoto([showPhoto[0] + 1, showPhoto[1]]);
+          setShowPhoto([showPhoto[0] + 1, showPhoto[1]]);
       }
-    }
+    } 
   }
 
   return(
     <div className='showImage-container' onClick={handleImgOnClick}>
       <i className="fas fa-times"/>
-
       {<div className='img-frame'>
         <i className="fas fa-angle-left" style={showPhoto[0] === 0 ? {color: 'rgba(0, 0, 0, 0)'} : null}/>
         <img src={showPhoto[1][showPhoto[0]]} alt={'answer-img'} onError={(e) => {
